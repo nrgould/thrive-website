ea445dd523863315fc6673c6400a1bda4104374e
nrgould <nrgould197@gmail.com>
2025-05-02 10:54:20 -0400

    added analytics from vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -39,6 +40,7 @@ export default function RootLayout({
 				<main>{children}</main>
 				<Footer />
 			</body>
+			<Analytics />
 		</html>
 	);
 }
