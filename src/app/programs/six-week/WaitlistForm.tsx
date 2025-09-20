b5200d1a97e37fa255c0117f48a7735c7b02601d
nrgould <nrgould197@gmail.com>
2025-09-20 15:15:22 -0400

    fixed button text

--- src/app/programs/six-week/WaitlistForm.tsx
+++ src/app/programs/six-week/WaitlistForm.tsx
@@ -178,7 +178,7 @@ export function WaitlistForm() {
 								size='lg'
 								disabled={loading}
 							>
-								{loading ? 'Submitting...' : 'Join Waitlist'}
+								{loading ? 'Submitting...' : 'Get Started'}
 							</Button>
 						</div>
 						<p className='text-xs text-slate-500 text-center'>
